85df30863a29c2fc158f4ea250c5110752c54b89
vibhavpd9 <41478219+vibhavpd9@users.noreply.github.com>
2018-10-15 17:32:17 +0530

    Update application/config/config.php

--- application/config/config.php
+++ application/config/config.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 
 
-$config['base_url'] = 'http://localhost/minicar/';        // Please type your Project URL;
+$config['base_url'] = 'http://localhost/';        // Please type your Project URL;
 
 /*
 |--------------------------------------------------------------------------
